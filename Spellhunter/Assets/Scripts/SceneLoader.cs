ad0281976256cce163a878bb0befd6f58e73602a
mootshell <mootshell@gmail.com>
2016-06-15 16:08:01 +1200

    Added basic scene flow

---
+++ Spellhunter/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public string scene;
+
+	public void Load()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
